e8df4c69fc3402add9360813823b04f703e981fc
Swapneel Bobba <swapneel.bobba@proton.me>
2024-05-04 10:18:38 -0700

    thirdweb setup

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+import {
+  PolygonZkevmCardonaTestnet,
+  BaseSepoliaTestnet,
+  MantaPacificSepoliaTestnet,
+} from "@thirdweb-dev/chains";
+
+export const ACTIVE_CHAIN = PolygonZkevmCardonaTestnet;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,31 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/layout";
+import {
+  ThirdwebProvider,
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  rainbowWallet,
+} from "@thirdweb-dev/react";
+
+import { ACTIVE_CHAIN } from "@/lib/constants";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThirdwebProvider
+      activeChain={ACTIVE_CHAIN}
+      clientId={process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}
+      supportedWallets={[
+        metamaskWallet(),
+        coinbaseWallet(),
+        walletConnect(),
+        rainbowWallet(),
+      ]}
+    >
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThirdwebProvider>
   );
 }

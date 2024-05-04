ecbc5966ac3b76abb6ccf2ee7de4428bb6051d95
Swapneel Bobba <swapneel.bobba@proton.me>
2024-05-04 03:51:06 -0700

    styling fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,14 +8,14 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center pt-36 ${inter.className}`}
     >
       <div className="flex flex-col place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
         <Image
-          className="-z-10 relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert -top-10 opacity-[0.04]"
+          className="-z-10 relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert -top-20 opacity-[0.04]"
           src="/images/swiftlaw-logo.svg"
           alt="Next.js Logo"
-          width={600}
+          width={700}
           height={37}
         />
         <div className="absolute mt-32">
@@ -34,7 +34,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {/* <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -102,7 +102,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   );
 }
